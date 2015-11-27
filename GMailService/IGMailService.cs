407b3d3b813e5ee1e93d5bed833422754caf8bb2
RGluschenko <r.gluschenko@gmail.com>
2015-11-27 16:57:03 +0200

    New commit

--- GMailService/IGMailService.cs
+++ GMailService/IGMailService.cs
@@ -27,21 +27,8 @@ namespace GMailService
     [DataContract]
     public class CompositeType
     {
-        bool boolValue = true;
-        string stringValue = "Hello ";
+       
 
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
 
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
     }
 }
